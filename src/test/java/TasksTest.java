e09ebf4c55a94f2640624c30caca8e4a5eceb069
Ekaterina Korotkova <ekaterinakorotkova300799@gmail.com>
2023-08-16 17:44:40 +0300

    Add new test

--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -20,7 +20,24 @@ public class TasksTest {
         Assertions.assertEquals(true, simpleTask.matches("Позвонить родителям"));
         Assertions.assertEquals(true, epic.matches("Яйца"));
         Assertions.assertEquals(true, meeting.matches("Приложение НетоБанка"));
+    }
 
+    @Test
+    public void shouldNoFindTaskByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Assertions.assertEquals(false, simpleTask.matches("Позвонить маме"));
+        Assertions.assertEquals(false, epic.matches("Сыр"));
+        Assertions.assertEquals(false, meeting.matches("Приложение Яндекс"));
     }
 }
