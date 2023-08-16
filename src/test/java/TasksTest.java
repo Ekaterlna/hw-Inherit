b26faa9a4df75e0433a603f4de896849e68a8bac
Ekaterina Korotkova <ekaterinakorotkova300799@gmail.com>
2023-08-16 17:38:57 +0300

    Init commit

---
+++ src/test/java/TasksTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    @Test
+    public void shouldFindTaskByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Assertions.assertEquals(true, simpleTask.matches("Позвонить родителям"));
+        Assertions.assertEquals(true, epic.matches("Яйца"));
+        Assertions.assertEquals(true, meeting.matches("Приложение НетоБанка"));
+
+
+    }
+}
